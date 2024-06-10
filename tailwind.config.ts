e66e4114120a74152c6588f5ce92f60105946e69
yehtetdev <167299865+yehtetdev@users.noreply.github.com>
2024-06-10 16:41:47 +0700

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,85 @@
 import type { Config } from "tailwindcss";
+import colors, { neutral, slate, transparent } from 'tailwindcss/colors'
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '0rem',
+        xs: '0rem',
+        sm: '0rem',
+        md: '0rem',
+        lg: '2rem',
+        xl: '4rem',
+        '2xl': '4rem',
+      },
+      screens: {
+        xs: '320px',
+        sm: '575px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1400px',
+      },
+    },
+
+    colors: {
+
+      // default colors
+      transparent: 'transparent',
+      gray: colors.gray,
+      slate: colors.slate,
+      neutral: colors.neutral,
+      indigo: colors.indigo,
+      black: colors.black,
+      white: colors.white,
+
+      // Grenadier primary color palette
+      primary: {
+        50: '#fff4ec',
+        100: '#ffe6d4',
+        200: '#ffc9a7',
+        300: '#ffa26f',
+        400: '#ff7035',
+        500: '#ff4a0e',
+        600: '#df2b04',
+        700: '#c81e06',
+        800: '#9f1a0d',
+        900: '#80180e',
+        950: '#450805',
+      },
+    },
+    
+    letterSpacing:{
+      tightest: '-.075em', 
+      tighter: '-.050em', 
+      tight: '-.025em', 
+      normal: '.0', 
+      wide: '.10em', 
+      wider: '.50em',
+      widest: '1.2em',  
+
+    },
+
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        // Add custom keyframes here
+      },
+      animation: {
+        // Add custom animations here
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
+
 export default config;
